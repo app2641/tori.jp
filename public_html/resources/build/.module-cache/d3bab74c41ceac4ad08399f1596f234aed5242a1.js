d9a51f9bf2750d12e50ceab58af455f9e69a4608
app2641 <app2641@gmail.com>
2015-04-14 07:11:38 +0900

    メディアクエリ実装

---
+++ public_html/resources/build/.module-cache/d3bab74c41ceac4ad08399f1596f234aed5242a1.js
@@ -0,0 +1,93 @@
+
+var App = {};
+App.VideoComponent = React.createClass({displayName: "VideoComponent",
+  propTypes: {
+    total: React.PropTypes.number.isRequired,
+    date: React.PropTypes.string.isRequired
+  },
+
+  getInitialState: function () {
+    return {
+      next_key: null,
+      movies: [
+        {user: null, caption: null, url: null},
+        {user: null, caption: null, url: null}
+      ],
+      movie_data: null
+    };
+  },
+
+  componentDidMount: function () {
+    var dates = this.props.date.split('-');
+    var url = "/contents/"+dates[0]+"/"+dates[1]+"/"+dates[2]+"/movies.json";
+
+    $.ajax({
+      url: url,
+      success: function (response) {
+        var data  = JSON.parse(response);
+        var rand  = this.getRandomNum(this.state.next_key);
+        var rand2 = this.getRandomNum(rand);
+
+        this.setState({
+          next_key: rand2,
+          movies: [data[rand], data[rand2]],
+          movie_data: data
+        });
+
+        // MediaElementPlayerの初期化
+        this.initMediaElementPlayer();
+      },
+      context: this
+    });
+  },
+
+  getRandomNum: function (base_int) {
+    var rand = base_int;
+    while (rand == base_int) {
+      rand = Math.floor(Math.random()* this.props.total);
+    }
+
+    return rand;
+  },
+
+  loadNextMovie: function () {
+    var rand = this.getRandomNum(this.state.next_key);
+    var data = this.state.movie_data;
+
+    this.setState({
+        next_key: rand,
+        movies: [this.state.movies[1], data[rand]],
+        movie_data: data
+    });
+  },
+
+  initMediaElementPlayer: function() {
+    var me = this;
+
+    // $('video').mediaelementplayer({
+        // enableAutoSize: true,
+        // alwaysShowControls: false
+    // });
+    $('video')[0].addEventListener('loadeddata', function (){
+      this.play()
+    });
+    $('video')[0].addEventListener('ended', function () {
+      me.loadNextMovie();
+    });
+  },
+
+  render: function () {
+    return (
+      React.createElement("video", {id: "video", width: "640", height: "640", autoplay: true}, 
+        React.createElement("source", {type: "video/mp4", src: this.state.movies[0].url}), 
+        React.createElement("source", {type: "video/mp4", src: this.state.movies[1].url})
+      )
+    );
+  }
+});
+
+React.render(
+  React.createElement(App.VideoComponent, {total: Info.total, date: Info.date}),
+  document.getElementById('video-container')
+);
+
